752c5b7623b44bb5635ee83c6eba2ea19bc06418
Aveesah H. Siddiqui <saveesah@gmail.com>
2026-01-08 23:44:15 +0530

    Added days of the week

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,11 +3,13 @@ import './calendar.css';
 
 export default function Calendar() {
     let cal = [];
+    //const days = Array.from({length: 7});
+    const days = ['Mon', 'Tues', 'Wed', 'Thrus', 'Fri', 'Sat', 'Sun']
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 7; i++) {
         let week = [];
-        for (let j = 0; j < 7; j++) {
-            week[j] = j + 1;
+        for (let j = 0; j < 5; j++) {
+            week[j] = i*5+j+1;
         }
         cal[i] = week;
     }
@@ -16,6 +18,11 @@ export default function Calendar() {
         <>
             <Navbar />
             <div className="calendar">
+
+                {days.map((day) => (
+                    <div className='days' >{day}</div>
+                ))}
+
                 {cal.map((week, weekIndex) => (
                     <div className="week" key={weekIndex}>
                         {week.map((date, dateIndex) => (
